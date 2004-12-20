bbfd7360afb18c29fb5828f91890e7028adf63c9
trans <trans@jabberwocky.ca>
2004-12-20 23:05:54 +0000

    Switched to links.inc file format

--- src/plugin/urler.pm
+++ src/plugin/urler.pm
@@ -15,6 +15,6 @@ sub do_urler_chat {
 	local($irc, $msg, $privs) = @_;
 
 	if ($msg->{'text'} =~ /(http\:|www\.)(\S*)/) {
-		csv_add($msg->{'respond'}, "url.lst", '', "<a href=\"$1$2\">$msg->{'text'}</a><br>");
+		csv_add($msg->{'respond'}, "url.lst", '', ",$1$2,$msg->{'text'},$msg->{'nick'}");
 	}
 }
