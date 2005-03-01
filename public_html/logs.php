d585b0c05c6b63364fdcbaed3a23553503fd5360
trans <trans@jabberwocky.ca>
2005-03-01 04:10:03 +0000

    Bug fixes

--- public_html/logs.php
+++ public_html/logs.php
@@ -1,10 +1,18 @@
+<html>
+<head>
+</head>
+<body>
 <?php
 	require_once("logs.inc");
 
-	if (isset(_GET['channel'])) {
+	if (isset($_GET['channel'])) {
 		print_logs($_GET['channel'], $_GET['date'], "");
 	}
 	else {
+		echo("<h2>Channels</h2>\n<hr>\n");
 		print_channels("");
+		echo("<hr>\n");
 	}
 ?>
+</body>
+</html>
