37754bd4a776210cda37706af57ddcb3e4687631
trans <trans@jabberwocky.ca>
2007-01-28 23:17:31 +0000

    Rewrote for new module system.

--- src/cmds/plugin.pm
+++ src/cmds/plugin.pm
@@ -1,28 +1,36 @@
 #
-# Command Name:	plugin.lm
-# Version:	0.1
-# Package:	Core
+# Command Name:	plugin.pm
 #
 
-$module_info = {
+my $module_info = {
 	'help' => [
-		"Usage: plugin <name>",
-		"Description: Loads the named plugin"
+		"Usage: plugin enable|disable <name>",
+		"Description: Enables or disables the named plugin"
 	]
 };
 
-sub do_plugin {
-	local($irc, $msg, $privs) = @_;
+sub do_command {
+	my ($irc, $msg, $privs) = @_;
 
-	return(-20) if (scalar(@{ $msg->{'params'} }) < 2);
-	($channel, $name) = @{ $msg->{'params'} };
-	return(-10) if (user_get_access($irc->{'users'}, $channel, $msg->{'nick'}) < 400);
+	return(-20) if (scalar(@{ $msg->{'args'} }) != 3);
+	return(-10) if ($privs < $irc->{'options'}->get_scalar_value("plugin_privs", 450));
+	my ($channel, $cmd, $name) = @{ $msg->{'args'} };
 
-	$name =~ s/\.pm$//;
-	return(-1) unless (-e "plugin/$name.pm");
-	channel_append_to_option($irc->{'channels'}, $channel, "plugins", $name);
-	return(-1) if (module_execute($irc, $msg, "$name.pm", "init_$name"));
-	irc_notice($irc, $msg->{'nick'}, "Plugin $name loaded successfully");
+	my $plugin = "plugins/$name/$name.pm";
+	return(-1) unless (-e $plugin);
+	if ($cmd eq "enable") {
+		$irc->{'options'}->add_value("plugins", $name);
+		module->load_plugin($plugin);
+		$irc->notice($msg->{'nick'}, "Plugin $name loaded successfully");
+	}
+	elsif ($cmd eq "disable") {
+		$irc->{'options'}->remove_value("plugins", $name);
+		module->release_plugin($plugin);
+		$irc->notice($msg->{'nick'}, "Plugin $name disabled");
+	}
+	else {
+		return(-20);
+	}
 	return(0);
 }
 
