92fcbff009e98f2c62aeb96c9003564495292c12
trans <trans@jabberwocky.ca>
2007-01-28 21:37:42 +0000

    Factored the logging out of irc.pm and made it a plugin.

---
+++ src/plugins/logging/cmds/search.pm
@@ -0,0 +1,29 @@
+#
+# Command Name:	search.pm
+#
+
+my $module_info = {
+	'help' => [
+		"Usage: search <phrase>",
+		"Description: Displays the website link for a logs search for phrase"
+	]
+};
+
+sub do_command {
+	my ($irc, $msg, $privs) = @_;
+
+	return(-20) if (scalar(@{ $msg->{'params'} }) < 2);
+	my $bot_site = $irc->{'options'}->get_scalar_value("bot_site", "");
+	return(0) unless ($bot_site);
+
+	$msg->{'phrase'} =~ /^\s*(.+)\s*$/;
+	my $phrase = $1;
+	$phrase =~ s/ /\+/g;
+	my $channel = $msg->{'respond'};
+	$channel =~ s/^\#+//;
+	$bot_site =~ s/(\\|\/)$//;
+	$irc->private_msg($msg->{'respond'}, "$bot_site/search.php?channel=$channel&q=$phrase");
+
+	return(0);
+}
+
