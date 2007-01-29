dda4a2b19f380d8b7e2822db4ca996d8204e7218
trans <trans@jabberwocky.ca>
2007-01-29 22:12:53 +0000

    Fixed to take an optional argument.

--- src/plugins/utils/cmds/mysite.pm
+++ src/plugins/utils/cmds/mysite.pm
@@ -15,9 +15,10 @@ sub do_command {
 	my $options = $irc->{'channels'}->get_options($msg->{'args'}->[0]);
 	$options = $irc->{'options'} unless ($options);
 	my $website = $options->get_scalar_value("usersite");
+	my $nick = $msg->{'args'}->[1] ? $msg->{'args'}->[1] : $msg->{'nick'};
 
 	if ($website) {
-		$irc->private_msg($msg->{'respond'}, "$website?t=users/$msg->{'nick'}");
+		$irc->private_msg($msg->{'respond'}, "$website?t=users/$nick");
 	}
 	else {
 		$irc->notice($msg->{'nick'}, "Sorry, No Website For This User");
