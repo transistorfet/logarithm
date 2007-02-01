a7715276127cb1d58513b06212162d4fd7d0c1bd
trans <trans@jabberwocky.ca>
2007-02-01 01:45:12 +0000

    Added help command.

---
+++ src/cmds/help.pm
@@ -0,0 +1,52 @@
+#
+# Command Name:	help.pm
+#
+
+sub get_info {{
+	'access' => 0,
+	'help' => [
+		"Usage: help [<command>]",
+		"Description: Displays a list of commands or the help description for a specified command"
+	]
+}}
+
+sub do_command {
+	my ($irc, $msg, $privs) = @_;
+
+	my @help = ();
+	if (scalar(@{ $msg->{'args'} }) > 1) {
+		if (my $info = module->get_info(module->get_command_package($msg->{'args'}->[1]))) {
+			@help = @{ $info->{'help'} };
+		}
+		else {
+			$irc->notice($msg->{'nick'}, "Sorry, No help available on $msg->{'args'}->[1]");
+			return(0);
+		}
+	}
+	else {
+		my $line;
+		my $i = 0;
+		my @commands = sort(module->get_command_list());
+		foreach my $command (@commands) {
+			$command = uc($command);
+			my $blank = "";
+			for (1..(15 - length($command))) {
+				$blank .= '.';
+			}
+			$line .= " [$command$blank]";
+			if (++$i >= 3) {
+				push(@help, $line);
+				$line = "";
+				$i = 0;
+			}
+		}
+		push(@help, $line) if ($line);
+	}
+
+	foreach (@help) {
+		$irc->notice($msg->{'nick'}, $_);
+	}
+
+	return(0);
+}
+
