b1d47bf59d17c296f233c62f341565755e466a01
trans <trans@jabberwocky.ca>
2007-02-08 04:21:43 +0000

    Modified to use the new irc->get_connections() function to get the $irc objects instead of relying on getting it at plugin startup.

--- src/plugins/topicifier/topicifier.pm
+++ src/plugins/topicifier/topicifier.pm
@@ -3,6 +3,7 @@
 # Description:	Changes the topic of the channel once a week to a random topic from a list
 #
 
+use irc;
 use misc;
 
 my $change_day = 5;
@@ -11,10 +12,10 @@ my $change_hour = 16;
 my $config_dir = "../etc";
 
 sub init_plugin {
-	my ($plugin_dir, $irc) = @_;
+	my ($plugin_dir) = @_;
 
 	my $info = { 'changed' => 0, 'channels' => { } };
-	module->register_timer("topic", 1800, 1, "check_time", $info, $irc);
+	module->register_timer("topic", 1800, 1, "check_time", $info);
 	module->register_command_directory("$plugin_dir/cmds");
 	return(0);
 }
@@ -24,11 +25,11 @@ sub release_plugin {
 }
 
 sub check_time {
-	my ($info, $irc) = @_;
+	my ($info) = @_;
 
 	my $time = get_time();
 	if (($time->{'wday'} == $change_day) and ($time->{'hour'} == $change_hour)) {
-		change_topic($info, $irc) unless ($info->{'changed'});
+		change_topic($info) unless ($info->{'changed'});
 		$info->{'changed'} = 1;
 	}
 	else {
@@ -37,17 +38,20 @@ sub check_time {
 }
 
 sub change_topic {
-	my ($info, $irc) = @_;
+	my ($info) = @_;
 
 	status_log("Topicifier changing topics");
-	$irc->identify();
-	foreach my $channel ($irc->{'channels'}->get_channel_list()) {
-		my $options = $irc->{'channels'}->get_options($channel);
-		next unless ($options);
-		next unless ($options->get_scalar_value("enable_topicifier"));
-		load_topics($info, $channel) unless (defined($info->{'channels'}->{ $channel }) and scalar(@{ $info->{'channels'}->{ $channel } }));
-		my $topic = shift(@{ $info->{'channels'}->{ $channel } });	
-		$irc->private_msg("chanserv", "topic $channel $topic") if ($topic);
+	my $connections = irc->get_connections();
+	foreach my $irc (@{ $connections }) {
+		$irc->identify();
+		foreach my $channel ($irc->{'channels'}->get_channel_list()) {
+			my $options = $irc->{'channels'}->get_options($channel);
+			next unless ($options);
+			next unless ($options->get_scalar_value("enable_topicifier"));
+			load_topics($info, $channel) unless (defined($info->{'channels'}->{ $channel }) and scalar(@{ $info->{'channels'}->{ $channel } }));
+			my $topic = shift(@{ $info->{'channels'}->{ $channel } });	
+			$irc->private_msg("chanserv", "topic $channel $topic") if ($topic);
+		}
 	}
 }
 
