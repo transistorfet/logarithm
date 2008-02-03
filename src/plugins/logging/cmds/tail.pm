a7bb523bed160a4014bcc0b6ddcb0b7c1bd5d156
transistor <trans@lorax.jabberwocky.ca>
2008-02-02 20:09:23 -0800

    Fixed properly.

--- src/plugins/logging/cmds/tail.pm
+++ src/plugins/logging/cmds/tail.pm
@@ -5,7 +5,7 @@
 sub get_info {{
 	'access' => 0,
 	'help' => [
-		"Usage: tail [-<lines>] [<channel>]",
+		"Usage: tail [<channel>] [<lines>]",
 		"Description: Sends the last 10 lines of the channel logs (current if unspecified) as a notice."
 	]
 }}
@@ -15,16 +15,18 @@ sub do_command {
 
 	my @data;
 	my $time = get_time();
-	my $channel = $msg->{'args'}->[0];
+	return(-20) unless (scalar(@{ $msg->{'args'} }) <= 2);	
+	my ($channel, $lines) = @{ $msg->{'args'} };
 
+	$lines =~ s/^-//;
 	$channel =~ s/^#+//;
 	my $file = sprintf("$irc->{'logging'}->{'logdir'}/$channel/%02d-%02d-%02d.txt", $time->{'year'}, $time->{'month'}, $time->{'day'});
-	open(FILE, "$file") or (status_log("Cannot Open Log $file") and return(-1));
-	while (my $data = <FILE>) {
-		unshift(@lines, $data);
+	open(FILE, $file) or (status_log("Cannot Open Log $file") and return(-1));
+	while (my $line = <FILE>) {
+		unshift(@data, $line);
 	}
 	close(FILE);
-	for (1..5) {
+	for (1..$lines) {
 		$irc->notice($msg->{'nick'}, shift(@data));
 	}
 	return(0);
