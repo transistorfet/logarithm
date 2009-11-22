e7a8f7bec8fdd41fca61ada38c6f98a385b15e37
transistor <trans@lorax.jabberwocky.ca>
2009-11-21 18:11:53 -0800

    Added decide command.

---
+++ src/plugins/safp/cmds/decide.pm
@@ -0,0 +1,27 @@
+#
+# Command Name:	decide.pm
+#
+
+use Misc;
+
+sub get_info {{
+	'access' => 1,
+	'help' => [
+		"Usage: decide <option> or <option> [or ...]",
+		"Description: Randomly selects an option from a number of options"
+	]
+}}
+
+sub do_command {
+	my ($irc, $msg, $privs) = @_;
+
+	return(-20) if (scalar(@{ $msg->{'args'} }) < 2);
+	my $list = $msg->{'phrase'};
+	$list =~ s/\s*(.*)\s*(|\r)\n$/\1/;
+	return if ($list =~ /^$/);
+
+	my @options = split(/\s+or\s+/, $list);
+	my $number = int(rand(scalar(@options)));
+	$irc->private_msg($msg->{'respond'}, "$options[$number]");
+}
+
